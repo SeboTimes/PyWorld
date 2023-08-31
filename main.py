87f3e0ec8e0c76bca2f76165f18ff57bd73d9bb4
Max00141 <max805284@gmail.com>
2023-08-31 16:23:52 +0200

    Autp
    Auto Brum Brum

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
 import pygame
 
-(width, height) = (300, 200)
+black = (0,0,0)
+white = (255,255,255)
+(width, height) = (800, 600)
+
+
 screen = pygame.display.set_mode((width, height))
 pygame.display.flip()
-pygame.display.set_caption('Sebo hatn kleinen')
-background_colour = (255,255,255)
-screen.fill(background_colour)
+pygame.display.set_caption('Sebo hatn')
+screen.fill(black)
+clock = pygame.time.Clock()
+
+carImg = pygame.image.load("auto.jpeg")
+
+def car(x,y):
+  screen.blit(carImg,(x,y))
+
+x = (width * 0.45)
+y = (height * 0.8)
 
 running = True
 while running:
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       running = False
+    
+  screen.fill(white)
+  car(x, y)
+  pygame.display.update()
+  car(x,y)
+  clock.tick(60)
