8b366dda2e7b01efab3ace9b9f82747ff4ce15d4
SeboTimes <sebochrisczemp@gmail.com>
2023-08-31 15:56:00 +0200

    Main

---
+++ main.py
@@ -0,0 +1,14 @@
+import pygame
+
+(width, height) = (300, 200)
+screen = pygame.display.set_mode((width, height))
+pygame.display.flip()
+pygame.display.set_caption('Fortnite')
+background_colour = (255,255,255)
+screen.fill(background_colour)
+
+running = True
+while running:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      running = False
