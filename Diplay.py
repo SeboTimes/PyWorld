a6201a99e9514e0cf6f19ff81359c0e060c8324d
SeboTimes <sebochrisczemp@gmail.com>
2023-08-31 16:25:34 +0200

    Display Update 1

---
+++ Diplay.py
@@ -0,0 +1,14 @@
+import pygame
+
+screen: pygame.Surface
+
+class Display:
+    def __init__(self, width: int, height: int):
+        screen = pygame.display.set_mode((width, height))
+        pygame.display.flip()
+        pygame.display.set_caption('Sebo hatn kleinen')
+        background_colour = (255,255,255)
+        screen.fill(background_colour)
+
+    def resulution():
+        pass
